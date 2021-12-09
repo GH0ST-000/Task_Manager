f64b7355a43e6b1b32959948e5d32f1115b22a46
Mac <mac@MacBook-Pro.local>
2021-12-09 20:55:25 +0400

    Design

--- resources/views/Form/login.blade.php
+++ resources/views/Form/login.blade.php
@@ -8,15 +8,6 @@
 </head>
 <body>
 
-<div class="grid grid-cols-3">
-    <div class="flex w-48 h-10 rounded-md bg-indigo-300 text-white mt-1">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-3 ml-2 bi bi-skip-backward" viewBox="0 0 16 16">
-            <path d="M.5 3.5A.5.5 0 0 1 1 4v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v2.94l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L8.5 8.752v2.94c0 .653-.713.998-1.233.696L1 8.752V12a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm7 1.133L1.696 8 7.5 11.367V4.633zm7.5 0L9.196 8 15 11.367V4.633z"/>
-        </svg>
-        <a href="/" class="ml-4 mt-2">
-            Back to Welcome
-        </a>
-    </div>
     <div>
         <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div class="max-w-md w-full space-y-8">
